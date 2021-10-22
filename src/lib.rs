a3d03f5f1eca5b8c512e905034e2bd88b91bbed8
adsick <adsick@protonmail.com>
2021-10-22 12:37:05 +0300

    added some initial yet not tested code

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::LookupMap;
-use near_sdk::{Promise, env, near_bindgen, AccountId, *};
+use near_sdk::{env, json_types::U128, near_bindgen, AccountId, Promise, *};
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
@@ -15,60 +15,62 @@ impl Default for MakeWallets {
 #[near_bindgen]
 impl MakeWallets {
     //falls when I try to view it.
-    pub fn make_wallet(account: AccountId, amount: json_types::U128)->Promise {
-        let p = Promise::new("adsick.testnet".parse().unwrap())
-            .create_account()
-            .transfer(amount.0);
+    //idk should it be payable or not, works without it tho
+    pub fn make_wallets(accounts: Vec<AccountId>, amounts: Vec<U128>) -> Promise {
+        if accounts.is_empty() {
+            env::panic_str("you need to provide account list")
+        } else if accounts.len() != amounts.len() {
+            env::panic_str("the length list of initial deposits is not equal to list af accounts") //todo make better message
+        }
 
-            p
+        let accounts = accounts
+            .into_iter()
+            .map(|a| AccountId::new_unchecked(format!("{}.{}", a, env::current_account_id()))); //todo delete for not creating subaccounts
+        let amounts = amounts.into_iter().map(|a| a.0);
+
+        let len = accounts.len();
+
+        let mut ps: Vec<Promise> = Vec::with_capacity(len);
+
+        for (ac, am) in accounts.zip(amounts){
+            ps.push(Promise::new(ac).create_account().add_full_access_key(env::signer_account_pk()).transfer(am));
+        }
+        
+        ps.into_iter().reduce(|a, b|{a.then(b)}).unwrap()
     }
 }
 
-// #[cfg(not(target_arch = "wasm32"))]
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use near_sdk::MockedBlockchain;
-//     use near_sdk::{testing_env, VMContext};
+#[cfg(not(target_arch = "wasm32"))]
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use near_sdk::{testing_env, VMContext};
 
-//     fn get_context(input: Vec<u8>, is_view: bool) -> VMContext {
-//         VMContext {
-//             current_account_id: "alice_near".to_string(),
-//             signer_account_id: "bob_near".to_string(),
-//             signer_account_pk: vec![0, 1, 2],
-//             predecessor_account_id: "carol_near".to_string(),
-//             input,
-//             block_index: 0,
-//             block_timestamp: 0,
-//             account_balance: 0,
-//             account_locked_balance: 0,
-//             storage_usage: 0,
-//             attached_deposit: 0,
-//             prepaid_gas: 10u64.pow(18),
-//             random_seed: vec![0, 1, 2],
-//             is_view,
-//             output_data_receivers: vec![],
-//             epoch_height: 0,
-//         }
-//     }
+    fn get_context(input: Vec<u8>, is_view: bool) -> VMContext {
+        VMContext {
+            current_account_id: "alice_near".to_string(),
+            signer_account_id: "bob_near".to_string(),
+            signer_account_pk: vec![0, 1, 2],
+            predecessor_account_id: "carol_near".to_string(),
+            input,
+            block_index: 0,
+            block_timestamp: 0,
+            account_balance: 0,
+            account_locked_balance: 0,
+            storage_usage: 0,
+            attached_deposit: 0,
+            prepaid_gas: 10u64.pow(18),
+            random_seed: vec![0, 1, 2],
+            is_view,
+            output_data_receivers: vec![],
+            epoch_height: 0,
+        }
+    }
 
-//     #[test]
-//     fn set_get_message() {
-//         let context = get_context(vec![], false);
-//         testing_env!(context);
-//         let mut contract = StatusMessage::default();
-//         contract.set_status("hello".to_string());
-//         assert_eq!(
-//             "hello".to_string(),
-//             contract.get_status("bob_near".to_string()).unwrap()
-//         );
-//     }
-
-//     #[test]
-//     fn get_nonexistent_message() {
-//         let context = get_context(vec![], true);
-//         testing_env!(context);
-//         let contract = StatusMessage::default();
-//         assert_eq!(None, contract.get_status("francis.near".to_string()));
-//     }
-// }
+    #[test]
+    fn create_accounts() {
+        let context = get_context(vec![], false);
+        testing_env!(context);
+        let mut contract = MakeWallets::default();
+    }
+}
